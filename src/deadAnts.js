cc15df55d332d803e7d781414e9de43ac0ca87ee
Heydi Garcia Sanchez <courantluna@gmail.com>
2024-02-26 20:51:32 -0400

    First commit & version 1 of the deadAnts Counter

---
+++ src/deadAnts.js
@@ -0,0 +1,26 @@
+/* Version 1. DeadAnts Counter from a string */
+/* Heydi 1107303*/
+
+function countDeadAnts(input) {
+    // Removes all occurrences of "ant" to leave only the remains that could be dead ants
+    const remains = input.replace(/ant/g, '');
+    
+    // Counts the occurrences of each relevant character: 'a', 'n', 't'
+    const count = { a: 0, n: 0, t: 0 };
+    for (const character of remains) {
+      if (count.hasOwnProperty(character)) {
+        count[character]++;
+      }
+    }
+    
+    // Finds the maximum number of occurrences among 'a', 'n', and 't'
+    const maxDeadAnts = Math.max(count.a, count.n, count.t);
+    
+    return maxDeadAnts;
+  }
+  
+  // Example of usage
+  const input = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t";
+  const deadAnts = countDeadAnts(input);
+  console.log(`there are a total of ${deadAnts} ants dead`);; // Expected output according to the given input
+  
